e93fd466a3744ecdc0710b9053aab631bfa96249
Juan Camacho <juanc@tiv.solutions>
2021-03-18 14:51:30 -0400

    added another comment example

--- migrations/20210318135437-create-user.js
+++ migrations/20210318135437-create-user.js
@@ -30,3 +30,27 @@ module.exports = {
     // await queryInterface.renameTable('owners', 'users');
   }
 };
+
+// up: async (queryInterface, Sequelize) => {
+//   await queryInterface.addColumn(
+//     'Orders', // name of Source model
+//     'CustomerId', // name of the key we're adding 
+//     {
+//       type: Sequelize.UUID,
+//       references: {
+//         model: 'Customers', // name of Target model
+//         key: 'id', // key in Target model that we're referencing
+//       },
+//       onUpdate: 'CASCADE',
+//       onDelete: 'SET NULL',
+//     }
+//   );
+// },
+
+// down: async (queryInterface, Sequelize) => {
+//   await queryInterface.removeColumn(
+//     'Orders', // name of Source model
+//     'CustomerId' // key we want to remove
+//   );
+// }
+// };
